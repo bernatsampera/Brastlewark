830777305126b906482b4bac7718c1c913bcf23c
bernatsampera <b.sampera@gmail.com>
2020-07-04 10:43:43 +0200

    Create citizen detail

---
+++ src/app/components/dashboard/citizen-detail/citizen-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CitizenDetailComponent } from './citizen-detail.component';
+
+describe('CitizenDetailComponent', () => {
+  let component: CitizenDetailComponent;
+  let fixture: ComponentFixture<CitizenDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CitizenDetailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CitizenDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/dashboard/citizen-detail/citizen-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-citizen-detail',
+  templateUrl: './citizen-detail.component.html',
+  styleUrls: ['./citizen-detail.component.scss']
+})
+export class CitizenDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
